9c99587636d6f89ba5b4c9d912ac1dc041e7da24
AlexandruTerescenco <alexandruterescencp@gmail.com>
2025-01-18 15:23:50 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proiect_Final_TerescencoAlexandru.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
